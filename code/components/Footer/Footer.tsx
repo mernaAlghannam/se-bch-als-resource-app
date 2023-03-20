55aa02e23d80d0cd349d06185ae8629be8c93db7
marsof02 <66377681+marsof02@users.noreply.github.com>
2023-03-20 04:00:26 -0400

    feat: finish initial footer

--- code/components/Footer/Footer.tsx
+++ code/components/Footer/Footer.tsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import { createStyles, Text, Container, rem } from '@mantine/core';
+import Image from 'next/image'
+import image12 from '../../src/styles/image12.png'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+const useStyles = createStyles((theme) => ({
+  footer: {
+    position: 'absolute',
+    width: '100%',
+    left:0,
+    bottom:0, 
+    marginTop: rem(120),
+    paddingTop: `calc(${theme.spacing.xl} * 2)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 2)`,
+    backgroundColor: '#254885',
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  logo: {
+    maxWidth: rem(200),
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  groups: {
+    display: 'flex',
+    flexWrap: 'wrap',
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  wrapper: {
+    width: rem(160),
+  },
+
+  link: {
+    display: 'block',
+    color: '#FFFFFF',
+    fontFamily:'Inter',
+    fontStyle:'normal',
+    fontWeight:400,
+    fontSize: '12px',
+    lineHeight:'15px',
+    paddingTop: rem(3),
+    paddingBottom: rem(3),
+
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+}));
+
+interface FooterLinksProps {
+  data: {
+    links: { label: string; link: string }[];
+  }[];
 }
 
-export default Footer
+export function FooterLinks({ data }: FooterLinksProps) {
+  const { classes } = useStyles();
+
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text<"a">
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        // onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper}>
+        {links}
+      </div>
+    );
+  });
+  console.log(groups)
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          {/* original size settings */}
+          <Image alt='Best hospitals logo' src={image12} width={69.5} height={80}/>
+          {/* slightly bigger image, could fit better on desktop site */}
+          {/* <Image alt='Best hospitals logo' src={image12} size={20}/> */}
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+    </footer>
+  );
+}
--- code/src/pages/CommunicationPage.tsx
+++ code/src/pages/CommunicationPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FooterLinks } from "../../components/Footer/Footer"
 
 const CommunicationPage = () => {
   return (
-    <div>CommunicationPage</div>
+    <div>CommunicationPage
+      <FooterLinks data={[{links:[{label:"About Us", link: "https://www.childrenshospital.org/about-us"}, {label:"Virtual Visits", link: "https://www.childrenshospital.org/programs/virtual-visits"},{label:"MyChildren's Patient Portal", link: "https://apps.childrenshospital.org/mychildrens/index.html"},{label:"Quality and Patient Safety",link: "https://www.childrenshospital.org/about-us/patient-safety-quality"},{label:"Career Opportunities",link: "https://www.childrenshospital.org/career-opportunities"},{label:"Locations",link: "https://www.childrenshospital.org/about-us/locations"},{label:"Education & Training",link: "https://dme.childrenshospital.org/"}]}]}/>
+    </div>
   )
 }
 
