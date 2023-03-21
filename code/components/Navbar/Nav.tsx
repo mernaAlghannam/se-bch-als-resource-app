0dbaf0ecb3fd9b3f656dad70ed85d44550439af0
chien-ying <b06505032@ntu.edu.tw>
2023-03-21 18:08:16 -0400

    add bgColor in navbar

--- code/components/Navbar/Nav.tsx
+++ code/components/Navbar/Nav.tsx
@@ -9,7 +9,8 @@ const useStyles = createStyles(() => ({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-  }  
+    backgroundColor: 'white'
+  }
 }))
 
 
@@ -17,13 +18,13 @@ const Nav = () => {
   const { classes } = useStyles();
     return (
       <Header height={HEADER_HEIGHT} sx={{ borderBottom: 0 }} mb={120}>
-      <Container className={classes.inner} fluid>
-        <Group>
-          <Image maw={100} mah={120} ml={32} mx="auto" radius="md" src="../Boston_Children's_Hospital_logo..png" alt="Random image" />
-        </Group>
-        <Burger size="lg" color="#254885" />
-      </Container>
-    </Header>
+        <Container className={classes.inner} fluid>
+          <Group>
+            <Image maw={100} mah={120} ml={32} mx="auto" radius="md" src="../Boston_Children's_Hospital_logo..png" alt="Random image" />
+          </Group>
+          <Burger size="lg" color="#254885" />
+        </Container>
+      </Header>
   )
   
 }
