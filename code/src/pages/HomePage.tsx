2c8f450e976d3968dcd10dd4ee9fc562f94dfb4a
ArkashJ <arkjain@bu.edu>
2023-03-31 09:41:49 -0400

    making the buttons for the body

--- code/src/pages/HomePage.tsx
+++ code/src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import FooterTitle from '../../components/Footer/Titles'
+import { FooterLinks } from "../../components/Footer/Footer"
+import Nav from '../../components/Navbar/Nav'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Nav></Nav>
+      <FooterTitle titleImg={"/titleimghome.PNG"} title={"Home"} />
+      <FooterLinks data={[{links:[{label:"About Us", link: "https://www.childrenshospital.org/about-us"}, {label:"Virtual Visits", link: "https://www.childrenshospital.org/programs/virtual-visits"},{label:"MyChildren's Patient Portal", link: "https://apps.childrenshospital.org/mychildrens/index.html"},{label:"Quality and Patient Safety",link: "https://www.childrenshospital.org/about-us/patient-safety-quality"},{label:"Career Opportunities",link: "https://www.childrenshospital.org/career-opportunities"},{label:"Locations",link: "https://www.childrenshospital.org/about-us/locations"},{label:"Education & Training",link: "https://dme.childrenshospital.org/"}]}]}/>
+     </div>
   )
 }
-
 export default HomePage
+
+
