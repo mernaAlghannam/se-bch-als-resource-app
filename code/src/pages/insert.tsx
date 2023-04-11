148dfe3426810bb19d2bddb7307ea913b4ec7f75
ArkashJ <arkjain@bu.edu>
2023-04-11 15:15:19 -0400

    function to insert data into the backend

---
+++ code/src/pages/insert.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const Insert = () => {
+    const [data, setData] = useState({
+        name: "",
+        age: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:3000/api/insert", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error("Something went wrong!");
+        }
+
+        const result = await response.json();
+        console.log(result);
+
+        if (result.status === "success") {
+            alert("Data inserted successfully!");
+        }
+
+        setData({ name: "", age: "" });
+
+    }
+
+}
